e3dc3cdedee150a93b808d1f009afa9404ad6e31
Elmer Torres <Sofnito@gmail.com>
2022-09-29 23:23:32 -0500

    middleware
    middleware

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('profiles','ProfileController@index')->name('profiles.index');
-Route::post('profiles','ProfileController@store')->name('profiles.store');
-Route::get('profiles/create','ProfileController@create')->name('profiles.create');
-Route::get('profiles/{profile}','ProfileController@show')->name('profiles.show');
-Route::match(['PUT','PATCH'],'profiles/{profile}', 'ProfileController@update')->name('profiles.update');
-Route::get('profiles/{profile}/edit','ProfileController@edit')->name('profiles.edit');
-Route::delete('profiles/{profile}','ProfileController@destroy')->name('profiles.delete');
+Route::get('profiles','ProfileController@index')->name('profiles.index')->middleware('auth');
+Route::post('profiles','ProfileController@store')->name('profiles.store')->middleware('auth');
+Route::get('profiles/create','ProfileController@create')->name('profiles.create')->middleware('auth');
+Route::get('profiles/{profile}','ProfileController@show')->name('profiles.show')->middleware('auth');
+Route::match(['PUT','PATCH'],'profiles/{profile}', 'ProfileController@update')->name('profiles.update')->middleware('auth');
+Route::get('profiles/{profile}/edit','ProfileController@edit')->name('profiles.edit')->middleware('auth');
+Route::delete('profiles/{profile}','ProfileController@destroy')->name('profiles.delete')->middleware('auth');
 
 Route::get('/home', 'HomeController@index')->name('home');
